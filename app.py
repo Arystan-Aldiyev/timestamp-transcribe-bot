54bc027d84b82df35c68568d4db522f7ca2e4fac
Arystan-Aldiyev <aldiyev.ars@gmail.com>
2022-11-13 22:41:45 +0600

    YouTube handler

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from calendar import c
-from email import message
-from urllib import response
-import aiogram
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ContentType
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ContentType, InputFile
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import Bot, Dispatcher, types, executor
@@ -12,6 +8,8 @@ import logging
 import json
 import time
 import requests
+from pytube import YouTube, exceptions
+import os
 
 conf = open('config.json')
 data = json.load(conf)
@@ -19,16 +17,19 @@ data = json.load(conf)
 API_KEY = data["MODEL"]
 API_TOKEN = data["BOT"]
 
-BTN_Youtube = InlineKeyboardButton('YouTube', callback_data='youtube')
-BTN_Ted = InlineKeyboardButton('Ted', callback_data='ted')
+BTN_Youtube = InlineKeyboardButton(text='YouTube', callback_data='youtube')
+BTN_Ted = InlineKeyboardButton(text='Ted', callback_data='ted')
 Standard = InlineKeyboardMarkup().add(BTN_Youtube, BTN_Ted)
 
 
-def start(path):
-    print("Okay I started")
+def start(url, source):
+    if source == 'tg':
+        audio_url = f"https://api.telegram.org/file/bot{API_TOKEN}/{url}"
+    if source == 'yt':
+        audio_url = url
     endpoint = "https://api.assemblyai.com/v2/transcript"
     json = {
-        "audio_url": f"https://api.telegram.org/file/bot{API_TOKEN}/{path}",
+        "audio_url": audio_url,
         "auto_chapters": True,
         "speaker_labels": True,
         "language_detection": True
@@ -56,7 +57,6 @@ def polling(id):
     response = get_response(id)
     print("I tried again")
     if response[0].json()['status'] == 'completed':
-        print("LETSGOOOOOOO")
         res = {}
         res['transcript'] = response[0].json()['text']
         res['chapters'] = response[0].json()['chapters']
@@ -123,9 +123,29 @@ def checklink(url):
     return 'BadLink'
 
 
-def download_yt():
-    pass
+def download_yt(url):
+    try:
+        yt = YouTube(url)
+    except exceptions.PytubeError:
+        return 'error'
+    else:
+        filename = yt.streams.filter(
+            only_audio=True).first().download()
 
+        def read_file(filename, chunk_size=5242880):
+            with open(filename, 'rb') as _file:
+                while True:
+                    data = _file.read(chunk_size)
+                    if not data:
+                        break
+                    yield data
+
+        headers = {'authorization': API_KEY}
+        response = requests.post('https://api.assemblyai.com/v2/upload',
+                                 headers=headers,
+                                 data=read_file(filename))
+        os.remove(filename)
+        return response.json()["upload_url"]
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -169,13 +189,11 @@ async def handle_media(message: Message):
         file = await message.video.get_file()
     if content == 'video_note':
         file = await message.video_note.get_file()
-    print("okay let's go")
     filepath = file.file_path
-    id = start(filepath)
+    id = start(filepath, 'tg')
     await bot.send_message(chat_id=message.chat.id, text='Started uploading to the server')
     results = polling(id)
     while len(results) == 0:
-        print(":( sleep")
         await bot.send_message(chat_id=message.chat.id, text=f'Waiting for the audio to be transcribed...')
         time.sleep(5)
         results = polling(id)
@@ -184,73 +202,39 @@ async def handle_media(message: Message):
         await bot.send_message(chat_id=message.chat.id, text=msg)
     for msg in ans['paras']:
         await bot.send_message(chat_id=message.chat.id, text=msg)
-    # msg = 'Содержание:\n\n'
-    # i = 0
-    # for item in results['chapters']:
-    #     sStart = item['start'] / 1000
-    #     mStart = 0
-    #     if sStart > 60:
-    #         mStart = sStart // 60
-    #         sStart -= mStart * 60
-    #     sEnd = item['end'] / 1000
-    #     mEnd = 0
-    #     if sEnd > 60:
-    #         mEnd = sEnd // 60
-    #         sEnd -= mEnd * 60
-    #     msg += f"{mStart}:{sStart} --- {mEnd}:{sEnd}\nHeadline: \"{item['headline']}\"\n\nGist: \"{item['gist']}\"\n\nSummary: \"{item['summary']}\"\n\n"
-    #     i += 1
-    #     if i % 3 == 0:
-    #         await bot.send_message(chat_id=message.chat.id, text=msg)
-    #         msg = ''
-    # if len(msg) > 0:
-    #     await bot.send_message(chat_id=message.chat.id, text=msg)
-    # msg = '\nПараграфы:\n\n'
-    # i = 0
-    # for item in results['paras']:
-    #     sStart = item['start'] / 1000
-    #     mStart = 0
-    #     if sStart > 60:
-    #         mStart = sStart // 60
-    #         sStart -= mStart * 60
-    #     sEnd = item['end'] / 1000
-    #     mEnd = 0
-    #     if sEnd > 60:
-    #         mEnd = sEnd // 60
-    #         sEnd -= mEnd * 60
-    #     msg += f"{mStart}:{sStart} --- {mEnd}:{sEnd}\n: \"{item['text']}\"\n\n"
-    #     i += 1
-    #     if i % 3 == 0:
-    #         await bot.send_message(chat_id=message.chat.id, text=msg)
-    #         msg = ''
-    # if len(msg) > 0:
-    #     await bot.send_message(chat_id=message.chat.id, text=msg)
-    print("Done")
 
 
-@dp.callback_query_handler(text='youtube')
+@dp.callback_query_handler(text=['youtube', 'ted'])
 async def process_callback_weather(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.delete_message(callback_query.from_user.id,
+                             callback_query.message.message_id)
+    user = callback_query.from_user.id
+    await bot.answer_callback_query(callback_query.id)
+    async with state.proxy() as proxy:
+        if callback_query.data == 'youtube':
+            await bot.send_message(user, text="Скачиваю видео с YouTube")
+            print(f'{proxy["name"]} wants video from {proxy["url"]}')
+            audio_url = download_yt(proxy["url"])
+        if callback_query.data == 'ted':
+            await bot.send_message(user, text="Ted дисн гой. Еще нету")
     # state = dp.current_state(user=callback_query.from_user.id)
     # await state.set_state(TestStates.all()[0, 'http.youtube.com tuda suda'])
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, text="Ютуб дисн гой")
-    async with state.proxy() as proxy:
-        await bot.send_message(chat_id=callback_query.from_user.id, text=f'{proxy["name"]} хочет видос по ссылке {proxy["url"]}')
 
-
-@dp.callback_query_handler(text='ted')
-async def process_callback_weather(callback_query: types.CallbackQuery, state: FSMContext):
-    # state = dp.current_state(user=callback_query.from_user.id)
-    # await state.set_state(TestStates.all()[1, 'http.tedx tuda suda'])
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, text="Тед ток дисн гой")
-    async with state.proxy() as proxy:
-        await bot.send_message(chat_id=callback_query.from_user.id, text=f'{proxy["name"]} хочет видос по ссылке {proxy["url"]}')
-
-
-@dp.message_handler(commands=['state'])
-async def showState(state: FSMContext):
-    async with state.proxy() as proxy:
-        await bot.send_message(chat_id=message.chat.id, text=f'Name: {proxy["name"]}\n url: {proxy["url"]}')
+    if audio_url == 'error':
+        await bot.send_message(chat_id=user, text='Ошибка.')
+    else:
+        id = start(audio_url, 'yt')
+        await bot.send_message(chat_id=user, text='Started uploading to the server')
+        results = polling(id)
+        while len(results) == 0:
+            await bot.send_message(chat_id=user, text=f'Waiting for the audio to be transcribed...')
+            time.sleep(5)
+            results = polling(id)
+        ans = generate_message(results)
+        for msg in ans['chapters']:
+            await bot.send_message(chat_id=user, text=msg)
+        for msg in ans['paras']:
+            await bot.send_message(chat_id=user, text=msg)
 
 
 @dp.message_handler(content_types=ContentType.ANY)
@@ -288,5 +272,4 @@ async def get_message(message: Message, state: FSMContext):
 
 
 if __name__ == "__main__":
-    print("Bot is ready")
     executor.start_polling(dp)
